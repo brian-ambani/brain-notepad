7ab1d62baf86e4e8c23a24b15d1ad4133c8ec7e4
Brian Ambani <brainambani1@gmail.com>
2023-01-12 01:56:15 +0300

    updated the pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brain Notepad</title>
+    <title>contact us</title>
 </head>
 <body>
-    <h1>This is the homepage for the Notepad</h1>
-    <p>Your notes will be here in future</p>
-    <a href="/about">About</a><br><br>
-    <a href="/contact">Contact us</a>
+    <h1>This page contains my contact details</h1>
+    <p>my contacts will appear here</p>
+    <a href="/">Home</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('/about', "about");
+
+Route::view('/contact', "contact");
